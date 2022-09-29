e9f5410191cfd8d5294c324adf3e7fc0b9d497dd
BeyondConfused <114650102+BeyondConfused@users.noreply.github.com>
2022-09-29 13:36:57 +0200

    Added semicolons to solve vscode errors

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function preload() {
   api = "e812164ca05ed9e0344b89ebe273c141";
   json = loadJSON(url(city, api));
   json2 = loadJSON("https://api.open-meteo.com/v1/forecast?latitude=" + round(json.coord.lat) + "&longitude=" + round(json.coord.lon) + "&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Europe%2FBerlin");
-}
+};
 
 function setup() {
   createCanvas(1920 * prop, 3000 * prop);
@@ -28,8 +28,8 @@ function setup() {
 function draw() {
   background(255);
   fill(0);
-  menuline()
-  fpage()
+  menuline();
+  fpage();
   
 }
 
@@ -49,7 +49,7 @@ function refresh() {
   let sunSetTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
  
   // Testing area
-  console.log("Current city: "+city)
+  console.log("Current city: "+city);
   console.log("Current tmp is: " + tmp + "°");
   console.log("Current weather: " + weather);
   console.log("Sunset is at: " + sunSetTime);
@@ -78,29 +78,23 @@ function keyPressed() {
   }
 }
 
-function seasonFish() {
 
-
-
-
-  
-}
 
 function menuline() {
-let spacing = 260
-let hspace = 120
-fill(0,0,240)
-rect(0,0,width,hspace*prop)
-strokeWeight(2)
-line(hspace*prop, -hspace*prop, hspace*prop, hspace*prop)
+let spacing = 260;
+let hspace = 120;
+fill(0,0,240);
+rect(0,0,width,hspace*prop);
+strokeWeight(2);
+line(hspace*prop, -hspace*prop, hspace*prop, hspace*prop);
 for (let i = 0; i < 9; i++) {
-line((hspace*prop)+(spacing*prop)*i,-spacing*prop,(hspace*prop)+spacing*prop*i,hspace*prop)
+line((hspace*prop)+(spacing*prop)*i,-spacing*prop,(hspace*prop)+spacing*prop*i,hspace*prop);
 }
-line(0,hspace*prop,width,hspace*prop)
-strokeWeight(1)
+line(0,hspace*prop,width,hspace*prop);
+strokeWeight(1);
 }
 function fpage(){
-stroke()
-fill(255,255,0)
-rect(200,100,300,300)
+stroke(0);
+fill(255,255,0);
+rect(200,100,300,300);
 }
