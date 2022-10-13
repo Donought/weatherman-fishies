7ced4acf307f0d1a716163225ad2c603cc85c408
Donought <114650357+Donought@users.noreply.github.com>
2022-10-13 19:08:06 +0200

    It works for now

--- futureTemp.js
+++ futureTemp.js
@@ -1,25 +1,52 @@
 function futureTemp() {
-  function cLine(d) {
+  function fLine(a, b, c) {
     stroke(0);
-    line(x + 20, y + d, x + w - 20, y + d);
+    line(x + 20, y + c, x + b, y + c);
   }
-  function cText(txt, o, d, s) {
+  function fText(txt, a, b, s) {
+    textAlign(LEFT, CENTER);
     textSize(s);
     noStroke();
     fill(0);
-    text(txt, x + o, y + d);
+    text(txt, x + a, y + b);
   }
+  function fBox(a, b, c, d) {
+    stroke(0);
+    noFill();
+    rect(x + a, y + b, c, d);
+  }
+
   x = 450;
   y = 200;
 
-  w = 800;
+  w = 1000;
   h = 650;
 
-  stroke(0);
-  noFill();
-  rect(x, y, w, h);
+  fBox(0, 0, w, h);
 
-  cText("Fremtidige temperaturer", 20, 40, 30);
+  fText("Vejr i fremtiden", 30, 45, 40);
+  //fLine(20, w - 20, 60);
 
-  cLine(60);
+  for (i = 0; i < 5; i++) {
+    a = (h - 110) / 5;
+    fBox(20, 90 + a * i, w - 40, a);
+    fText(json2.daily.time[i + 1], 60, 90 + a * i + a * 0.5, 30);
+
+    fBox(240, 100 + a * i, 390, a - 20);
+    image(tempImg, x + 250, y + 110 + a * i, a - 40, a - 40);
+    fText(
+      "Min: " + json2.daily.temperature_2m_min[i + 1] + "°",
+      330,
+      90 + a * i + a * 0.5,
+      30
+    );
+    fText(
+      "Max: " + json2.daily.temperature_2m_max[i + 1] + "°",
+      470,
+      90 + a * i + a * 0.5,
+      30
+    );
+
+    fBox(640, 100 + a * i, w - 670, a - 20);
+  }
 }
